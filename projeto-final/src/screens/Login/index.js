7e6019cc2aef43d1f48f3d767082e840515f0244
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-07-06 19:44:32 -0300

    validação de login feito em grupo

--- projeto-final/src/screens/Login/index.js
+++ projeto-final/src/screens/Login/index.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MainContainer } from '../../components/MainContainer/styles';
 import { FormContainer, Title, BeeIcon, EnterButton, ButtonText, WrapperLogin } from './styles';
 import { LoginInput, PasswordInput } from "../../components/LoginInput";
 import { api } from '../../services/api.js';
 
 export const Login = ({ navigation }) => {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
 
-
-
+  const getUsers = async () => {
+    const result = await api.get(`/usuario/login?login=${login}&senha=${password}
+    `).then((response) => {
+      if (response.data === 'Login confirmado!') {
+        navigation.navigate('Welcome')
+      }
+      else {
+        alert('Login e/ou senha inválidos.');
+      }
+    });
+  }
 
   return (
-    <MainContainer style={{}}>
+    <MainContainer>
       <WrapperLogin>
         <BeeIcon source={require('../../../assets/abelha.png')} />
         <FormContainer>
           <Title>Login</Title>
           <LoginInput
-            secureTextEntry
+            onChangeText={(text) => setLogin(text)}
             placeholder={'Digite seu login'}
           />
           <PasswordInput
             secureTextEntry
+            onChangeText={(text) => setPassword(text)}
             placeholder={'Digite sua senha'}
           />
-          <EnterButton onPress={() => navigation.navigate('Welcome')}>
+          <EnterButton onPress={getUsers}>
             <ButtonText>ENTRAR</ButtonText>
           </EnterButton>
         </FormContainer>
